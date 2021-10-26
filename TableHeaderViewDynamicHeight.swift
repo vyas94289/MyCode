b97287f1458af4700d1bf69729f88fbfa7fdaa66
Gaurang Vyas <vsgaurang@gmail.com>
2021-10-26 12:31:58 +0530

    Create TableHeaderViewDynamicHeight.swift

---
+++ TableHeaderViewDynamicHeight.swift
@@ -0,0 +1,13 @@
+override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        if let headerView = tableView.tableHeaderView {
+            let height = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+            var headerFrame = headerView.frame
+            //Comparison necessary to avoid infinite loop
+            if height != headerFrame.size.height {
+                headerFrame.size.height = height
+                headerView.frame = headerFrame
+                tableView.tableHeaderView = headerView
+            }
+        }
+    }
